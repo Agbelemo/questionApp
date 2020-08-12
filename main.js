a4b6f5d5fc5187b83cee610c27d3197b34fb2f95
Agbelemo <agbelemosetonji@gmail,com>
2020-08-12 23:37:20 +0100

    creating small  app with javascrip

---
+++ main.js
@@ -0,0 +1,7 @@
+function details(){
+    var name = prompt("Please what is your name");
+    var age = prompt("How old are you");
+    var result = "Hi " + name + " Welcome to Question app, You said you are " + age + ",Years old to continue please follow 'samueldamilare7' on instagram thanks";
+    document.write(result);
+};
+details();
